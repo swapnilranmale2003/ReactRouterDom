c2608214b6b7c63e963b8b11fb3bd11cd2557ca0
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-02-02 10:49:26 +0530

    routing of Home,about page

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const Home = ()=>{
+  return(
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+const About = ()=>{
+  return(
+    <div>
+      <h1>About Page</h1>
+    </div>
+  )
+}
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
